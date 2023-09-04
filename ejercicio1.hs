3c87b3ee330e4cc80165a88aebc3c09e5849b334
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-04 15:04:34 -0300

    Agrego el ejercicio 1 (a)

--- ejercicio1.hs
+++ ejercicio1.hs
@@ -1,2 +1,12 @@
-funcion :: Int -> Int 
-funcion  x = x + x
+--Ejercicio 1.
+ --a) Implentar la funci´on parcial f :: Integer ->Integer 
+ --definida por extensi´on de la siguiente manera:
+--f(1) = 8
+--f(4) = 131
+--f(16) = 16
+
+f:: Int -> Int 
+f  x | x == 1 = 8 
+     | x == 4 = 131
+     | x == 16 = 16 
+     

48d8456b8ded19da6c5738d895c5e39b3aa615f9
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-04 15:18:43 -0300

    Agrego el ejercicio 1 b

--- ejercicio1.hs
+++ ejercicio1.hs
@@ -9,4 +9,14 @@ f:: Int -> Int
 f  x | x == 1 = 8 
      | x == 4 = 131
      | x == 16 = 16 
-     
+
+--b) Analogamente, especificar e implementar la funcion 
+--parcial g :: Integer ->Integer
+--g(8) = 16
+--g(16) = 4
+--g(131) = 1
+
+g:: Int -> Int 
+g  x | x == 8 = 16 
+     | x == 16 = 4
+     | x == 131 = 1 

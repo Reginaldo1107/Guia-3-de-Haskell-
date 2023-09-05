6a784327440a6ed75f830b6aafdef101daa915c8
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-05 10:40:44 -0300

    Agregi eb ejercicio 2 c

--- ejercicio2.hs
+++ ejercicio2.hs
@@ -12,3 +12,14 @@ maximoAbsoluto :: Integer -> Integer ->Integer
 maximoAbsoluto x y | valorAbsoluto x <= valorAbsoluto y  = valorAbsoluto y
                    | otherwise = valorAbsoluto x --valorAbsoluto x > valorAbsoluto y  = x
                    
+--c) maximo3: devuelve el máximo entre tres números enteros.
+--- creo una funcion que compare dos numeros y devuelva el maximo 
+maximoDe2 :: Int -> Int -> Int
+maximoDe2 x y | x <= y = y
+              |otherwise = x 
+
+maximoDe3 :: Int ->Int -> Int ->Int
+maximoDe3 x y z =  maximoDe2 (maximoDe2 x y)  z
+
+
+

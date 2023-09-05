0e957ca444b3468b88b229ca2ffe732ed810890a
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-05 13:34:26 -0300

    Agrego el ejercicio 4 b c d

--- ejercicio4.hs
+++ ejercicio4.hs
@@ -7,3 +7,26 @@
 
 prodInt :: (Float , Float) -> (Float , Float) -> Float 
 prodInt  (x, y) (w ,z) = x * w + y * z 
+
+--b) todoMenor: dadas dos tuplas R x R, decide si 
+--es cierto que cada coordenada de la primera 
+--tupla es menor a la coordenada correspondiente 
+-- de la segunda tupla
+todoMenor :: (Float , Float) -> (Float , Float) -> Bool 
+todoMenor (x , y) (w , z) = (x < w && y < z) 
+
+
+--c)distanciaPuntos: calcula la distancia entre 
+--dos puntos de R°2
+
+distanciaPuntos :: (Float , Float) -> (Float , Float) -> Float 
+distanciaPuntos (x, y) (w, z) = sqrt ((x - w) ^ 2 + (y - z) ^ 2)
+
+--d) sumaTerna: dada una terna de enteros, calcula 
+--la suma de sus tres elementos.
+sumaTerna:: (Int , Int , Int) -> Int
+sumaTerna (x,y,z) = x + y + z
+
+
+
+

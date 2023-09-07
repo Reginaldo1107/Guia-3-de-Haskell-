6a579ba926953293c51719e109716c68fe438c4f
Reginaldo1107 <reginaldohinojosabaldera@gmail.com>
2023-09-07 16:15:37 -0300

    Agrego el ejercicio7.hs

---
+++ ejercicio7.hs
@@ -0,0 +1,3 @@
+--Ejercicio 7. Implementar una función:
+distanciaManhattan:: (Float, Float, Float) ->(Float, Float, Float) ->Float
+distanciaManhattan (x, y, z) (a , b, c) = abs (x -a) + abs (y - b) + abs (z -c) 
